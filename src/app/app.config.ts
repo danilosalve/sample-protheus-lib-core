173caef5d17628742d179637c0f2e553f423c1d4
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2026-08-04 16:28:01 -0300

    feat(thf): adiciona novo tema totvs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { PoHttpRequestModule } from '@po-ui/ng-components';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProtheusLibCoreModule } from '@totvs/protheus-lib-core';
+import { ProtheusLibTHFModule } from '@framesp/protheus-lib-thf';
 
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { routes } from './app.routes';
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi(), withInterceptors([authInterceptor])),
-    importProvidersFrom([BrowserAnimationsModule, PoHttpRequestModule, ProtheusLibCoreModule]),
+    importProvidersFrom([BrowserAnimationsModule, PoHttpRequestModule, ProtheusLibTHFModule]),
     provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };
